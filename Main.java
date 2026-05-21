0787394bad321cb8f07df42ecdba306ee37936b5
RafaGamero808 <onbassrafa@gmail.com>
2026-05-21 20:44:02 +0200

    Initialize Mascota and start timer thread
    Initialize Mascota and start TemporizadorMascota thread.

--- Main.java
+++ Main.java
@@ -1,8 +1,12 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Mascota miMascota = new Mascota();
+        
+        // Arrancamos el hilo del tiempo
+        TemporizadorMascota temporalizador = new TemporizadorMascota(miMascota);
+        Thread hiloTiempo = new Thread(temporalizador);
+        hiloTiempo.start();
 
+        System.out.println("Mascota iniciada en etapa: " + miMascota.getEtapaEvolutiva());
     }
-    Mascota gato = new Mascota();
 }
